95d857e201c25d232fa76cd11da2a4d4165cf64a
Mark Thomas <markthethomas@gmail.com>
2016-03-04 10:01:54 -0800

    test: set up basic tests

---
+++ test/avatar.spec.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+
+import Avatar from '../lib/avatar';
+
+
+describe('<Avatar />', () => {
+  
+});
---
+++ test/gravatar.spec.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import Gravatar from '../lib/gravatar';
+
+describe('<Gravatar />', () => {
+
+});
