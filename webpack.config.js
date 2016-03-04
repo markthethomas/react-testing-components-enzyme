d3d06c5a840591ffb84a457ab726f4ab5d0b936b
Mark Thomas <markthethomas@gmail.com>
2016-03-04 09:58:49 -0800

    setup: babel, webpack, etc.

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const webpack = require('webpack');
+const BellOnBundlerErrorPlugin = require('bell-on-bundler-error-plugin');
+
+// env
+const buildDirectory = './dist/';
+
+module.exports = {
+  externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+  },
+  entry: './lib/gravatar.jsx',
+  devServer: {
+    hot: true,
+    inline: true,
+    port: 7700,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(buildDirectory),
+    filename: 'app.js',
+    publicPath: 'http://localhost:7700/dist',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-0'],
+      },
+    }],
+  },
+  plugins: [
+    new BellOnBundlerErrorPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(true),
+    // new webpack.optimize.DedupePlugin(),
+    // new webpack.optimize.UglifyJsPlugin(),
+  ],
+};
