96f8a2e95d9630a4d462fec91255ac34ecec93e4
Matt Cowski <Matt.Cowski@gmail.com>
2016-08-13 23:31:23 -0500

    missing dependency and readme info. no need for bower_components

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # react-testing-components-enzyme
 Sample repo for a blog post on testing react components with enzyme
+
+## installation
+`npm i`
+
+## start
+`npm run dev:hot`
+
+## run tests
+`npm test`
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      exclude: /(node_modules|bower_components)/,
+      exclude: /bower_components/,
       loader: 'babel',
       query: {
         presets: ['react', 'es2015', 'stage-0'],
